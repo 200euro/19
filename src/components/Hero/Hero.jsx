9ee0680565777718992804de08adf9e7abd97ddd
200euro <AKMALKADYROV281@GMAIL.COM>
2024-11-24 15:27:43 +0500

    Add files via upload

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Hero.scss'
+
+const Hero = () => {
+   return (
+<section className="hero">
+   <div className="container">
+      <div className="hero wrapper"></div>
+   </div>
+</section>
+   )
+}
+
+export default Hero
